9206791d8b9aa43d3ff4c14b72f227b94c5f8d6b
omiinaya <omiinaya@gmail.com>
2020-06-07 01:33:46 -0400

    heroku?

---
+++ client/build/precache-manifest.c3852e01e9c1f07777ffe76c52426897.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c6fd8d07a886be33504",
+    "url": "/static/css/main.c20fe46b.chunk.css"
+  },
+  {
+    "revision": "7c6fd8d07a886be33504",
+    "url": "/static/js/main.33534e13.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "18222a4d317365805a87",
+    "url": "/static/js/2.a5114083.chunk.js"
+  },
+  {
+    "revision": "8272ae1a4677594a4a602eea144bbb0d",
+    "url": "/index.html"
+  }
+];
