94c8ef29868ef97876acd18a30987b3475a9fe7b
omiinaya <omiinaya@gmail.com>
2020-06-06 22:27:09 -0400

    filtering by age.

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import { find } from "../../index"
 import { showMaleOnly } from "../../index"
 import { showFemaleOnly } from "../../index"
+import { showByLocation } from "../../index"
+import { lessThanAge } from "../../index"
+import { moreThanAge } from "../../index"
 import "./style.css";
 
 function Search() {
@@ -13,6 +16,10 @@ function Search() {
       <div className="row">
         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
         </div>
+        <input type="text" id="input-bar"></input>
+        <button onClick={moreThanAge}>Older Than</button>
+        <button onClick={lessThanAge}>Younger Than</button>
+        <button onClick={showByLocation}>City</button>
         <button onClick={showMaleOnly}>Male</button>
         <button onClick={showFemaleOnly}>Female</button>
         <div id="content"></div>
--- client/src/index.js
+++ client/src/index.js
@@ -9,11 +9,12 @@ ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
 
 var results;
-var male = [];
-var female = [];
+var byGender = [];
+var byCity = [];
+var byAge = [];
 
 export function find() {
-    var queryURL = "https://randomuser.me/api/?results=20&nat=us";
+    var queryURL = "https://randomuser.me/api/?results=50&nat=us";
     return axios
         .get(queryURL).then(response => {
             results = response.data.results
@@ -39,37 +40,37 @@ export function newEmployeeRow() {
 }
 
 export function showMaleOnly() {
-    male = [];
+    byGender = [];
     for (var i = 0; i < results.length; i++) {
         if (results[i].gender === "male") {
-            male.push(results[i]);
+            byGender.push(results[i]);
         }
     }
     filteredMaleRow()
-    console.log(male);
+    console.log(byGender);
 }
 
 export function showFemaleOnly() {
-    female = [];
+    byGender = [];
     for (var i = 0; i < results.length; i++) {
         if (results[i].gender === "female") {
-            female.push(results[i]);
+            byGender.push(results[i]);
         }
     }
     filteredFemaleRow()
-    console.log(female);
+    console.log(byGender);
 }
 
 export function filteredMaleRow() {
     document.getElementById('content').innerText = "";
-    for (var i = 0; i < male.length; i++) {
+    for (var i = 0; i < byGender.length; i++) {
         const div = document.createElement('div');
         div.innerHTML = `
         <div id="employee-`+i+`
-            <p id="first-name-`+i+`">Name: `+male[i].name.first+" "+male[i].name.last+`</p>
-            <p id="number-`+i+`">Cell: `+male[i].cell+`</p>
-            <p id="email-`+i+`">Email: `+male[i].email+`</p>
-            <img id="image-`+i+`" src="`+male[i].picture.thumbnail+`">
+            <p id="first-name-`+i+`">Name: `+byGender[i].name.first+" "+byGender[i].name.last+`</p>
+            <p id="number-`+i+`">Cell: `+byGender[i].cell+`</p>
+            <p id="email-`+i+`">Email: `+byGender[i].email+`</p>
+            <img id="image-`+i+`" src="`+byGender[i].picture.thumbnail+`">
         </div>
         `;
         document.getElementById('content').appendChild(div);
@@ -78,14 +79,98 @@ export function filteredMaleRow() {
 
 export function filteredFemaleRow() {
     document.getElementById('content').innerText = "";
-    for (var i = 0; i < female.length; i++) {
+    for (var i = 0; i < byGender.length; i++) {
         const div = document.createElement('div');
         div.innerHTML = `
         <div id="employee-`+i+`
-            <p id="first-name-`+i+`">Name: `+female[i].name.first+" "+female[i].name.last+`</p>
-            <p id="number-`+i+`">Cell: `+female[i].cell+`</p>
-            <p id="email-`+i+`">Email: `+female[i].email+`</p>
-            <img id="image-`+i+`" src="`+female[i].picture.thumbnail+`">
+            <p id="first-name-`+i+`">Name: `+byGender[i].name.first+" "+byGender[i].name.last+`</p>
+            <p id="number-`+i+`">Cell: `+byGender[i].cell+`</p>
+            <p id="email-`+i+`">Email: `+byGender[i].email+`</p>
+            <img id="image-`+i+`" src="`+byGender[i].picture.thumbnail+`">
+        </div>
+        `;
+        document.getElementById('content').appendChild(div);
+    }
+}
+
+export function showByLocation() {
+    byCity = [];
+    var input = document.getElementById('input-bar').value;
+    for (var i = 0; i < results.length; i++) {
+        if (results[i].location.city === input) {
+            byCity.push(results[i]);
+        }
+    }
+    filteredByCity()
+    console.log(byCity);
+}
+
+export function filteredByCity() {
+    document.getElementById('content').innerText = "";
+    for (var i = 0; i < byCity.length; i++) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div id="employee-`+i+`
+            <p id="first-name-`+i+`">Name: `+byCity[i].name.first+" "+byCity[i].name.last+`</p>
+            <p id="number-`+i+`">Cell: `+byCity[i].cell+`</p>
+            <p id="email-`+i+`">Email: `+byCity[i].email+`</p>
+            <img id="image-`+i+`" src="`+byCity[i].picture.thumbnail+`">
+        </div>
+        `;
+        document.getElementById('content').appendChild(div);
+    }
+}
+
+export function lessThanAge() {
+    byAge = [];
+    var input = document.getElementById('input-bar').value;
+    for (var i = 0; i < results.length; i++) {
+        if (results[i].dob.age <= input) {
+            byAge.push(results[i]);
+        }
+    }
+    filteredLessThan()
+    console.log(byAge);
+}
+
+export function filteredLessThan() {
+    document.getElementById('content').innerText = "";
+    for (var i = 0; i < byAge.length; i++) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div id="employee-`+i+`
+            <p id="first-name-`+i+`">Name: `+byAge[i].name.first+" "+byAge[i].name.last+`</p>
+            <p id="number-`+i+`">Cell: `+byAge[i].cell+`</p>
+            <p id="email-`+i+`">Email: `+byAge[i].email+`</p>
+            <img id="image-`+i+`" src="`+byAge[i].picture.thumbnail+`">
+        </div>
+        `;
+        document.getElementById('content').appendChild(div);
+    }
+}
+
+export function moreThanAge() {
+    byAge = [];
+    var input = document.getElementById('input-bar').value;
+    for (var i = 0; i < results.length; i++) {
+        if (results[i].dob.age >= input) {
+            byAge.push(results[i]);
+        }
+    }
+    filteredMoreThan()
+    console.log(byAge);
+}
+
+export function filteredMoreThan() {
+    document.getElementById('content').innerText = "";
+    for (var i = 0; i < byAge.length; i++) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div id="employee-`+i+`
+            <p id="first-name-`+i+`">Name: `+byAge[i].name.first+" "+byAge[i].name.last+`</p>
+            <p id="number-`+i+`">Cell: `+byAge[i].cell+`</p>
+            <p id="email-`+i+`">Email: `+byAge[i].email+`</p>
+            <img id="image-`+i+`" src="`+byAge[i].picture.thumbnail+`">
         </div>
         `;
         document.getElementById('content').appendChild(div);
