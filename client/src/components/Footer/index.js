b4db191366bae870ebdc0530a53253a22453308b
omiinaya <omiinaya@gmail.com>
2020-06-07 06:49:37 -0400

    centering footer.

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -6,7 +6,9 @@ function Footer() {
     <div className="footer">
       <div className="row">
         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-          <p>Copyright © 2019 Omar Minaya. All rights reserved.</p>
+          <center>
+            <p>Copyright © 2019 Omar Minaya. All rights reserved.</p>
+          </center>
         </div>
       </div>
     </div>
